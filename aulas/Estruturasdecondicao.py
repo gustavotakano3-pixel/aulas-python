9fd731e1b1b1381b89c591c45fb2b78e05b2fb0d
Gustavo <gustavotakano3@gmai.com>
2026-09-03 20:35:27 -0300

    Conteúdo de operadores lógicos

---
+++ aulas/Estruturasdecondicao.py
@@ -0,0 +1,21 @@
+# IF e ELSE -> SE e SENÃO
+
+#CASE SENSITIVE -> E != e
+idade = int(input('Digite sua idade: '))
+# criando uma condição na execução do código
+if idade >= 18: # executa SE a resposta boleana for True
+    if idade >= 65:
+        print("Desculpe senhor, você não pode entrar nessa balada.")
+    else:
+        print("Você pode entrar nessa balada.")
+elif idade < 5:
+    print("Álem de não poder entrar, é menor de idade")
+
+print("\n")
+
+nome = input('Digite seu nome: ')
+
+if nome =="":
+print("Por favor digite um nome válido")
+else:
+    print("Ola"+ nome+"! Seja bem vindo a nossa balada.")
