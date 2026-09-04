75c44598694e6e0a653abcec86720038df193c43
Gustavo <gustavotakano3@gmai.com>
2026-09-03 21:31:19 -0300

    lições

--- aulas/Estruturasdecondicao.py
+++ aulas/Estruturasdecondicao.py
@@ -2,20 +2,21 @@
 
 #CASE SENSITIVE -> E != e
 idade = int(input('Digite sua idade: '))
+
 # criando uma condição na execução do código
 if idade >= 18: # executa SE a resposta boleana for True
     if idade >= 65:
         print("Desculpe senhor, você não pode entrar nessa balada.")
     else:
         print("Você pode entrar nessa balada.")
-elif idade < 5:
+elif idade < 5: #ELSE + IF -> Elif
     print("Álem de não poder entrar, é menor de idade")
-
-print("\n")
 
 nome = input('Digite seu nome: ')
 
 if nome =="":
-print("Por favor digite um nome válido")
+    print("Por favor digite um nome válido")
+elif nome == "Gustavo":
+    print("Olha só, o dono da balada chegou!")
 else:
     print("Ola"+ nome+"! Seja bem vindo a nossa balada.")
--- aulas/OperadoresAritimeticos.py
+++ aulas/OperadoresAritimeticos.py
@@ -1,19 +1,23 @@
-#Operadores Matemáticos
+#OPERADORES MATEMÁTICOS
 numero1 = 10
 numero2 = 20
 
-#soma
+#SOMA
 soma = numero1 + numero2
-#subtração
+#SUBTRAÇÃO
 subtracao = numero1 - numero2
-#multiplicação
+#MULTIPLICAÇÃO
 multiplicacao = numero1 * numero2
-#divisão
-divisao = numero1 / numero2
-resto = numero1 % numero2
+#DIVISÃO
+divisao = numero1 / numero2 #3 / 2 -> 1.5
+#DIVISÃO SEM NUMERO QUEBRADO
+divisao_sem_numero_quebrado = numero1 // numero2 #3 / 2 -> 1
+#RESTO
+resto = numero1 % numero2 # 11 % 2 -> 1
 
-print ("Soma dos números: ", soma)
+print("Soma dos números: ", soma)
 print("Subtração dos números: ", subtracao)
-print("Multiplicação dos números", multiplicacao)
+print("Multiplicação dos números: ", multiplicacao)
 print("Divisão dos números: ", divisao)
-print("Resto dos números ", resto)
+print("Divisão dos números: ", divisao_sem_numero_quebrado)
+print("Resto dos números: ", resto)
