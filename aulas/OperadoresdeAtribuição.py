4958998afdeafeeac6dd725d7dc16e48d5447996
Gustavo <gustavotakano3@gmai.com>
2026-09-08 21:08:19 -0300

    salvando

---
+++ aulas/OperadoresdeAtribuição.py
@@ -0,0 +1,17 @@
+# OPERADORES DE ATRIBUIÇÃO
+# = -> atribui um valor à variável
+# += -> atribuir um valor X NUMÉRICO à variável
+# -= -> subtrair um valor X NUMÉRICO da variável
+# *= -> mutiplicar um valor X pelo NÚMERO da variável
+# /= -> dividir um valor X pelo NÚMERO da variável
+
+idade = 10
+print("O valor da idade é: ",idade)
+idade += 8 # 18 -> incremento
+print("O valor da idade é: ",idade)
+idade -= 8 # 10 -> decremento
+print("O valor da idade é: ",idade)
+idade *= 2 # 20
+print("O valor da idade é: ",idade)
+idade /= 2 # 10
+print("O valor da idade é: ",idade)
