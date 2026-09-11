ddf044460c2a898361234b6e0ffd5e74658a60a0
Gustavo <gustavotakano3@gmai.com>
2026-09-10 21:48:56 -0300

    salvando

---
+++ atividades/Atividade_7/Questão1.py
@@ -0,0 +1,12 @@
+print("Listas")
+
+funcionarios = []
+
+add = input("Quer adicionar Funcionários?")
+
+while True:
+    name = input("Digite o Nome do Funcionário:")
+    if name == 0:
+
+        break
+    funcionarios.append(add)
